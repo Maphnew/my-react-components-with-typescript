a51de25aa149e20963432d16ef87293c3a785ef4
maphnew <zcm3118@gmail.com>
2021-11-14 21:36:59 +0900

    New Todo, Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
+
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo'
 import './App.css';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos([...todos, newTodo])
+  }
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
